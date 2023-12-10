c49224ad804da207e19164d0cc69808acca7e7e1
Vlad <vlad.nedelcioiu@stud.electro.upb.ro>
2023-12-10 23:54:32 +0200

    Streams

---
+++ src/Exemplul5.java
@@ -0,0 +1,10 @@
+import java.util.stream.Stream;
+
+public class Exemplul5 {
+    public static void main(String[] args) {
+        Stream<String> stringStream = Stream.of("1","2","3");
+        int sum = stringStream.mapToInt(num -> Integer.parseInt(num)).sum();
+                            //.mapToInt(Integer::parseInt).sum();
+        System.out.println(sum);
+    }
+}

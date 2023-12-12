13df12fd3204e35ccfabea9b193d310850c35ff6
Vlad <vlad.nedelcioiu@stud.electro.upb.ro>
2023-12-12 18:05:20 +0200

    ExecutorService

---
+++ src/Exemplul6.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Exemplul6 {
+    public static void main(String[] args) {
+
+        AtomicInteger atomicInteger = new AtomicInteger();
+        Runnable task = () -> {
+            for(int i = 0; i<500000 ; i++){
+                atomicInteger.incrementAndGet();
+                //System.out.println(atomicInteger);
+            }
+        };
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        service.submit(task);
+        service.shutdown();
+
+        try {
+
+            if (!service.awaitTermination(10, TimeUnit.SECONDS)) {
+                service.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            service.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println(atomicInteger.get());
+
+    }
+}
